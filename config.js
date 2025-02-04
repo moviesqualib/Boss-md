4a33dfaaa75c6c5a4c12ac7e8068662e279bf2b1
moviesqualib <moviesqualib007@gmail.com>
2025-02-04 12:12:12 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://qu.ax/rejHh.jpg"
+    ALIVE_MSG: process.env.ALIVE_MSG || "Boss-md is alive"
 };
